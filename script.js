a003c5c98a22c4e49033fc54fe5406e3c72ae8af
Ofp00 <127768687+Ofp00@users.noreply.github.com>
2024-02-26 18:37:23 +0100

    Change playGamme function and move console.logs down

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ function getComputerChoice() { // Function that randomly chooses rock paper or s
     }
 } 
 
-console.log(computerSelection);
-
 function playRound() {  // Change 'You won...' to console.log and make the return 'tie' ' player' or 'computer' like dev-emad-777 on github
     if (playerSelection === 'ROCK' && computerSelection === 'ROCK') {  
             console.log('Tie! Rock equals Rock');
@@ -48,14 +46,25 @@ function playRound() {  // Change 'You won...' to console.log and make the retur
         }
 }
 
-console.log(playRound(playerSelection, computerSelection)); // Returns the result of the round in textformat
-
-function playGame() {
-    if (playRound === 'computer') {
-        return 'pc won';
-    } else if (playRound === 'player') {
-        return 'player won';
+function playGame(){ // kikade pa dors coding schools video for denna funktion 
+    for (let i = 0; i < 5; i++){
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
     }
 }
 
+ /* function playGame() {
+    let  winner;
+    winner = playRound(playerSelection, computerSelection);
+    
+    if (winner == 'computer') {
+        return 'pc won';
+    } else if (winner == 'player') {
+        return 'player won';
+    }
+} */
+playGame();
+
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection)); // Returns the result of the round in textformat
 console.log(playerScore); // Returns the score of the player
